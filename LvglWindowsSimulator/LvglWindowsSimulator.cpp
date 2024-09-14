eb4306716709137b1653689fc94a19b0ceda3bff
nhotynski <n.hotynski@gmail.com>
2024-09-14 21:05:24 +0200

    test

--- LvglWindowsSimulator/LvglWindowsSimulator.cpp
+++ LvglWindowsSimulator/LvglWindowsSimulator.cpp
@@ -26,8 +26,8 @@ int main()
     bool simulator_mode = true;
     lv_display_t* display = lv_windows_create_display(
         L"LVGL Windows Simulator Display 1",
-        800,
-        480,
+      320,  //800,
+      240, //480,
         zoom_level,
         allow_dpi_override,
         simulator_mode);
@@ -78,7 +78,7 @@ int main()
     }
 
     lv_demo_widgets();
-    //lv_demo_benchmark();
+   // lv_demo_benchmark();
 
     while (1)
     {
